08b70129ac755a20c47d24ec47cd98c6ca0ee8df
George Granberry <george.granberry@gmail.com>
2023-05-16 12:16:03 +0200

    naive implementation

---
+++ naive.erl
@@ -0,0 +1,10 @@
+-module(naive).
+
+-compile([export_all,nowarn_export_all]).
+
+bench() -> {Time, _} = timer:tc(naive, run, []),
+            io:format("Time: ~p~n", [Time]).
+
+run() ->
+  crawl:crawl("http://www.chalmers.se/",3),
+  page_rank:page_rank().
